40896326b99dcabaa2f77b03e96d29ccf1f29341
Tim Gromeyer <sakul8826@gmail.com>
2023-03-01 22:11:31 +0100

    Add files

---
+++ plugins/testPlugin/translations_de_DE.ts
@@ -0,0 +1,12 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de_DE">
+<context>
+    <name>PluginTest</name>
+    <message>
+        <location filename="plugintest.cpp" line="12"/>
+        <source>test plugin</source>
+        <translation type="unfinished">plugin testen</translation>
+    </message>
+</context>
+</TS>
