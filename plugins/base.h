0519bd4be6cd05be15645b5d79f2dec97b8ea7c9
Tim Gromeyer <sakul8826@gmail.com>
2023-05-14 08:58:09 +0200

    Add documentation

--- plugins/base.h
+++ plugins/base.h
@@ -9,15 +9,37 @@
 class PluginBridge;
 #endif
 
+/**
+ * @brief The PluginInterface class is an abstract interface for plugin functionality.
+ *
+ * PluginInterface provides an interface for defining and implementing plugin functionality.
+ * Plugins that implement this interface can be loaded dynamically at runtime.
+ */
 class PluginInterface
 {
 public:
     virtual ~PluginInterface() = default;
 
-    virtual bool isValid(const QString &) = 0;
+    /**
+     * @brief isValid checks if a plugin is valid for the given input.
+     * @param input The input to check.
+     * @return True if the plugin is valid for the given input, false otherwise.
+     */
+    virtual bool isValid(const QString &input) = 0;
 
-    virtual void run(const QString &) = 0;
+    /**
+     * @brief run executes the plugin functionality.
+     * @param input The input to process.
+     */
+    virtual void run(const QString &input) = 0;
 
+    /**
+     * @brief setBridge sets the bridge for the plugin to communicate with the host application.
+     * @param b The PluginBridge object.
+     *
+     * The bridge is used for communication between the plugin and the host application.
+     * It allows the plugin to interact with the host application's functionality and services.
+     */
     inline void setBridge(PluginBridge *b)
     {
 #ifndef NO_BRIDGE
@@ -27,5 +49,11 @@ public:
     PluginBridge *bridge = nullptr;
 };
 
+/**
+ * @brief The PLUGIN_iid macro defines the interface ID for the PluginInterface.
+ *
+ * This macro defines the unique interface ID for the PluginInterface.
+ * It is used to identify and load the correct plugin at runtime.
+ */
 #define PLUGIN_iid "io.github.tim-gromeyer.voiceassistant.plugin/1.0"
 Q_DECLARE_INTERFACE(PluginInterface, PLUGIN_iid);
--- plugins/bridge.h
+++ plugins/bridge.h
@@ -11,6 +11,12 @@
 class QWidget;
 #endif
 
+/**
+ * @brief The PluginBridge class provides a bridge for communication between plugins and the host application.
+ *
+ * PluginBridge serves as a communication bridge between plugins and the host application.
+ * It allows plugins to interact with the host application's functionality and services like text to speech and voice recognition.
+ */
 class PluginBridge final : public QObject
 {
     Q_OBJECT
@@ -18,14 +24,32 @@ class PluginBridge final : public QObject
     friend class MainWindow;
 
 public:
+    /**
+     * @brief PluginBridge constructor.
+     * @param parent The parent object.
+     */
     inline explicit PluginBridge(QObject *parent = nullptr)
         : QObject(parent){};
+
+    /**
+     * @brief PluginBridge destructor.
+     *
+     * The destructor releases the mutex lock if acquired.
+     */
     inline ~PluginBridge() final
     {
         if (mutex.tryLock())
             mutex.unlock();
     }
 
+    /**
+     * @brief ask sends a request to the host application with a question and waits for an answer.
+     * @param text The question text.
+     * @return The answer received from the host application.
+     *
+     * This function sends a question to the host application and waits for a response.
+     * It blocks the plugin execution until an answer is received.
+     */
     inline QString ask(const QString &text)
     {
         mutex.lock();
@@ -40,6 +64,13 @@ public:
         return answer;
     };
 
+    /**
+     * @brief sayAndWait sends a speech request to the host application and waits until it completes.
+     * @param text The text to be spoken.
+     *
+     * This function sends a speech request to the host application and waits until the speech is completed.
+     * It blocks the plugin execution until the speech is finished.
+     */
     inline void sayAndWait(const QString &text)
     {
         if (text.isEmpty())
@@ -57,13 +88,51 @@ public:
     }
 
 public Q_SLOTS:
+    /**
+     * @brief say sends a speech request to the host application.
+     * @param text The text to be spoken.
+     *
+     * This function sends a speech request to the host application.
+     * It does not block the plugin execution.
+     */
     inline void say(const QString &text) { Q_EMIT _say(text, QPrivateSignal()); };
 
 Q_SIGNALS:
+    /**
+     * @brief _say signal emitted to send a speech request to the host application.
+     * @param text The text to be spoken.
+     * @param signal The private signal.
+     *
+     * This signal is emitted to send a speech request to the host application.
+     */
     void _say(const QString &, QPrivateSignal);
+
+    /**
+     * @brief _sayAndWait signal emitted to send a speech request to the host application and wait until it completes.
+     * @param text The text to be spoken.
+     * @param signal The private signal.
+     *
+     * This signal is emitted to send a speech request to the host application and wait until it completes.
+     */
     void _sayAndWait(const QString &, QPrivateSignal);
+
+    /**
+     * @brief _ask signal emitted to send a question to the host application and wait for an answer.
+     * @param text The question text.
+     * @param signal The private signal.
+     *
+     * This signal is emitted to send a question to the host application and wait for an answer.
+     */
+
     void _ask(const QString &, QPrivateSignal);
 
+    /**
+     * @brief useWidget signal emitted to send a widget to the host application for usage.
+     * @param widget The widget to be used.
+     *
+     * This signal is emitted to send a widget to the host application for usage.
+     * The host application can use the provided widget as needed.
+     */
     void useWidget(QWidget *);
 
 private:
